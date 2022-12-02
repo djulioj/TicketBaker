735891bc27d3bd92be7361c53fba9d0dc4973748
dplataj <dplataj@uninorte.edu.co>
2022-12-02 00:45:24 -0500

    BasicLoginAndSignIn
    Falta ajustar validaciones extra en los input - opcional
    
    Ajustar el nombre de los archivos
    
    Falta que post login se redireccione segun el tipo de usuario - prioritario
    
    ocultar botones de inicio de sesion - prioritario
    
    Revisar BD ajustar para agregar campo de propietario de evento (username as key value)

--- src/Components/log-inForm.js
+++ src/Components/log-inForm.js
@@ -1,12 +1,10 @@
 import { useRef } from 'react';
-import Form from 'react-bootstrap/Form';
 import '../Styles/loginForm.css';
 
 function FormSigIn() {
 
   const username=useRef()
   const password=useRef()
-  /*onst isVendor=useRef()*/
   const userType=useRef()
 
 
@@ -20,6 +18,7 @@ function FormSigIn() {
         }
         localStorage.setItem(username.current.value,JSON.stringify(userInfo))
         alert("Registro Exitoso")
+        window.location.replace("/")
       }else{
         alert("Este usuario ya existe")
       }  
@@ -33,10 +32,7 @@ function FormSigIn() {
         <p className='text'>Usuario</p>
         <input className='username' ref={username}></input>
         <p className='text'>Contraseña</p>
-
         <input className='pwd' type={"password"} ref={password}></input>
-        {/*<Form.Check type="checkbox" label="Vendedor" className='isVendor' ref={isVendor} />*/}
-        {/*<a className="sign-in" href="#">Iniciar sesión</a>*/}
         <p className='text'>Tipo de Usuario</p>
         <select className="userType" ref={userType}>
           <option value="Vendedor">Vendedor</option>
--- src/Components/sign-inForm.js
+++ src/Components/sign-inForm.js
@@ -1,16 +1,35 @@
-import { Link } from 'react-router-dom';
+import { useRef } from 'react';
 import '../Styles/signinForm.css';
 
 function FormSigIn() {
+
+  const username_login = useRef()
+  const password_login = useRef()
+
+  const logInValidation=()=>{
+    if(localStorage.getItem(username_login.current.value)!== null){
+      const userInfo= JSON.parse(localStorage.getItem(username_login.current.value))      
+      if(password_login.current.value === userInfo.contra){
+        alert("Inicio de sesion Exitoso")
+      }else{
+        alert("User or Password incorrect, try again")
+      }
+    }else{
+      alert("User or Password incorrect, try again")
+    }
+  }
+
+
+
   return (
     <div className='card'>
         <p className='title'>Inciar sesión</p>
         <hr />
         <p className='text'>Usuario</p>
-        <input className='user'></input>
+        <input className='user' ref={username_login}></input>
         <p className='text'>Contraseña</p>
-        <input className='user' type={"password"}></input>
-        <a className="sign-in2" href="#">Iniciar sesión</a>
+        <input className='user' type={"password"} ref={password_login}></input>
+        <a className="sign-in2" href="#" onClick={logInValidation}>Iniciar sesión</a>
         <a className="log-in2" href="#">Registrarse</a>
     </div>
     
