a5ca56eaa6f215c5c1f7c2e708f41084efba7f66
dplataj <dplataj@uninorte.edu.co>
2022-12-01 20:55:13 -0500

    SignIn

--- src/Components/log-inForm.js
+++ src/Components/log-inForm.js
@@ -1,18 +1,53 @@
-import Button from 'react-bootstrap/Button';
+import { useRef } from 'react';
 import Form from 'react-bootstrap/Form';
 import '../Styles/signinForm.css';
 
 function FormSigIn() {
+
+  const username=useRef()
+  const password=useRef()
+  /*onst isVendor=useRef()*/
+  const userType=useRef()
+
+
+  const saveInLocalStorage=()=>{
+    if(username.current.value&&password.current.value&&userType.current.value){
+      if(localStorage.getItem(username.current.value) === null){
+        const userInfo={
+          "usuario": username.current.value,
+          "contra": password.current.value,
+          "tipoDeUsuario": userType.current.value
+        }
+        localStorage.setItem(username.current.value,JSON.stringify(userInfo))
+        alert("Registro Exitoso")
+      }else{
+        alert("Este usuario ya existe")
+      }  
+    }
+  }
+
   return (
     <div className='card'>
         <p className='title'>Crear Cuenta</p>
         <hr />
         <p className='text'>Usuario</p>
-        <input className='user'></input>
+        <input className='username' ref={username}></input>
         <p className='text'>Contraseña</p>
+<<<<<<< Updated upstream
         <input className='user' type={"password"}></input>
         <Form.Check type="checkbox" label="Vendedor" className='check' />
         <a className="sign-in2" href="#">Iniciar sesión</a>
+=======
+        <input className='pwd' type={"password"} ref={password}></input>
+        {/*<Form.Check type="checkbox" label="Vendedor" className='isVendor' ref={isVendor} />*/}
+        {/*<a className="sign-in" href="#">Iniciar sesión</a>*/}
+        <p className='userTypeTag'>Tipo de Usuario</p>
+        <select className="tipoUsuario" ref={userType}>
+          <option value="Vendedor">Vendedor</option>
+          <option value="Comprador">Comprador</option>
+        </select>
+        <button onClick={saveInLocalStorage}>Registrarse</button>
+>>>>>>> Stashed changes
     </div>
   );
 }
